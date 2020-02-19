03414f60b5073d8c4de319c1ef5468a7f2b99114
awagawag49 <awagawag49@gmail.com>
2020-02-20 04:23:03 +0700

    ok

--- contact.js
+++ contact.js
@@ -1,6 +1,8 @@
-<style type="text/css">
-#ContactForm1,#ContactForm1 br{display:none}
-</style>
+<html>
+<head>
+<title>Contact</title>
+</head>
+<body>
 <script src="https://www.blogger.com/static/v1/widgets/2271878333-widgets.js" type="text/javascript"></script>
 <script type="text/javascript">
 //<![CDATA[
@@ -8,3 +10,5 @@ if (typeof(BLOG_attachCsiOnload) != 'undefined' && BLOG_attachCsiOnload != null)
 _WidgetManager._RegisterWidget('_ContactFormView', new _WidgetInfo('ContactForm1', 'footer1', null, document.getElementById('ContactForm1'), {'contactFormMessageSendingMsg': 'Sending...', 'contactFormMessageSentMsg': 'Your message has been sent.', 'contactFormMessageNotSentMsg': 'Message could not be sent. Please try again later.', 'contactFormInvalidEmailMsg': 'A valid email address is required.', 'contactFormEmptyMessageMsg': 'Message field cannot be empty.', 'title': 'Contact Form', 'blogId': '4503790436212648199', 'contactFormNameMsg': 'Name', 'contactFormEmailMsg': 'Email', 'contactFormMessageMsg': 'Message', 'contactFormSendMsg': 'Send', 'submitUrl': 'https://www.blogger.com/contact-form.do'}, 'displayModeFull'));
 //]]>
 </script>
+</body>
+</html>
